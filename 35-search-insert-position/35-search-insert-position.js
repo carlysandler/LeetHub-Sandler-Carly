d9d3368e264239fa6a14018da4c5c4733bb9b781
Carly Sandler <carly.sandler@gmail.com>
2022-05-07 14:41:52 -0400

    Time: 86 ms (36.49%), Space: 41.9 MB (86.64%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -3,16 +3,36 @@
  * @param {number} target
  * @return {number}
  */
+// var searchInsert = function(nums, target) {
+//     let min = 0, max = nums.length - 1;
+    
+//     while (min <= max) {
+//         let middle = Math.floor((min + max) / 2);
+//         if (nums[middle] < target) min = middle + 1;
+//         else if (nums[middle] > target) max = middle - 1;
+//         else return middle;
+//     }
+//     return min;
+    
+    
+// };
+
 var searchInsert = function(nums, target) {
-    let min = 0, max = nums.length - 1;
-    
-    while (min <= max) {
-        let middle = Math.floor((min + max) / 2);
-        if (nums[middle] < target) min = middle + 1;
-        else if (nums[middle] > target) max = middle - 1;
-        else return middle;
+    let right = nums.length
+    if (target < nums[0]) {
+        return 0
     }
-    return min;
     
+    if (nums[right-1] < target) {
+        return right
+    }
+    let l = 0
     
-};
+    while (l < right) {
+        if (nums[l] == target || nums[l] > target) {
+            return l
+        }
+        l++
+        
+    }
+}
