b973e53e183ead8ca76d5eb6a853c6d7449ef96b
Carly Sandler <carly.sandler@gmail.com>
2022-05-08 15:56:26 -0400

    Time: 91 ms (75.00%), Space: 50.1 MB (89.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,12 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maxSubArray = function(nums) {
+//     let prev = -Infinity
+//     let max = prev
+//     for(let i = nums.length - 1; i >= 0; i--){
+//         prev = Math.max(nums[i], nums[i] + prev)
+//         max = Math.max(max, prev)
+//     }
+//     return max
+// };
+
 var maxSubArray = function(nums) {
-    let prev = -Infinity
-    let max = prev
-    for(let i = nums.length - 1; i >= 0; i--){
-        prev = Math.max(nums[i], nums[i] + prev)
-        max = Math.max(max, prev)
+    let max = nums[0];
+    let subArray = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        // if current subArray is negative, throw it away. Otherwise keep adding to it
+        subArray = Math.max(nums[i], subArray + nums[i]);
+        max = Math.max(max, subArray)
     }
-    return max
-};
+    return max;
+}
