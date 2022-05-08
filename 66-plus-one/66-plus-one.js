992cd53383239e70b26add95108ac6d2d8e83230
Carly Sandler <carly.sandler@gmail.com>
2022-05-08 16:34:44 -0400

    Time: 98 ms (18.28%), Space: 41.6 MB (93.83%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    // Time complexity: O(N)
+    // Space complexity: O(1)
+    for (let i = digits.length -1; i >= 0; i--){
+        if (digits[i] === 9) {
+            digits[i] = 0;
+        }
+        else {
+            digits[i]++
+            return digits;
+        }
+    }
+    // we are here because all digits are nine
+    digits.unshift(1);
+    
+    return digits;
+    
+};
