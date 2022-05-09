e63e1dc6623985f0cd0497b85570d28b25e937f7
Carly Sandler <carly.sandler@gmail.com>
2022-05-08 22:24:24 -0400

    Time: 93 ms (48.40%), Space: 45.1 MB (8.63%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.js
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -10,28 +10,40 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {
-    let q = [root, root];
-    if (!root) return true;
+// var isSymmetric = function(root) {
+//     let q = [root, root];
+//     if (!root) return true;
     
     
-    // while root is not null
-    while (q.length) {
-        node1 = q.pop();
-        node2 = q.pop();
+//     // while root is not null
+//     while (q.length) {
+//         node1 = q.pop();
+//         node2 = q.pop();
         
-        if (node1 === null && node2 === null) continue;
-        if (node1 === null || node2 === null) return false; // this will recursively be left vs right to check after first iteration
-        if (node1.val !== node2.val) return false;
+//         if (node1 === null && node2 === null) continue;
+//         if (node1 === null || node2 === null) return false; // this will recursively be left vs right to check after first iteration
+//         if (node1.val !== node2.val) return false;
         
-        q.push(node1.left);
-        q.push(node2.right);
-        q.push(node1.right);
-        q.push(node2.left);
+//         q.push(node1.left);
+//         q.push(node2.right);
+//         q.push(node1.right);
+//         q.push(node2.left);
                 
         
-    }
-    return true;
+//     }
+//     return true;
     
-    
-};
+// };
+
+// recursive solution
+
+var isSymmetric = function(root) {
+    return isMirror(root, root);
+}
+var isMirror = function(root1, root2) {
+    if (root1 === null && root2 === null) return true;
+    if (root1 === null || root2 === null) return false;
+    return (root1.val === root2.val) 
+        && isMirror(root1.left, root2.right)
+        && isMirror(root1.right, root2.left);
+}
